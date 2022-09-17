f0f269ecfa016bf1decf19a5d2a62d170752d179
YoonsikOh5 <109320448+YoonsikOh5@users.noreply.github.com>
2022-09-17 21:46:14 +0900

    [Bronze V] Title: A+B - 3, Time: 4 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/10950. A＋B － 3/A＋B － 3.cc
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+void plusab(int a, int b) {
+    
+    cout << a + b << endl;
+}
+
+int main() {
+
+    int T;
+    cin >> T;
+
+    for (int i = 0; i < T; i++) {
+        int a, b;
+        cin >> a >> b;
+        plusab(a, b);
+    }
+
+}
