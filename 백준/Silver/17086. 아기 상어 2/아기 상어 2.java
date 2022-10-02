fdac11445cb20e83c1751a3b17481996c7e1cc33
YoonsikOh5 <109320448+YoonsikOh5@users.noreply.github.com>
2022-10-02 23:44:27 +0900

    [Silver II] Title: 아기 상어 2, Time: 96 ms, Memory: 12540 KB -BaekjoonHub

--- 백준/Silver/17086. 아기 상어 2/아기 상어 2.java
+++ 백준/Silver/17086. 아기 상어 2/아기 상어 2.java
@@ -16,15 +16,15 @@ public class Main {
 	static int map[][];
 	static int visited[][];
 	static int step[][];
-	static List<Shark> slist;
+	static List<RC> slist;
 	static int max_result;
 	static final Integer MAX_D = 3000;
 	
-	public static class Shark{
+	public static class RC{
 		int r;
 		int c;
 		
-		public Shark(int r, int c) {
+		public RC(int r, int c) {
 			super();
 			this.r = r;
 			this.c = c;
@@ -48,7 +48,7 @@ public class Main {
 				int cur = Integer.parseInt(st.nextToken());
 				map[r][c] = cur;
 				if(cur==1) {
-					slist.add(new Shark(r,c));
+					slist.add(new RC(r,c));
 				}
 			}			
 		}
@@ -56,18 +56,12 @@ public class Main {
 		for(int[] arr : step) {
 			Arrays.fill(arr, MAX_D);
 		}
-		for(int i = 0, size = slist.size(); i < size; i++) {
-			Shark cur = slist.get(i);
-			bfs(cur);
-//			for(int[] arr : step) {
-//				System.out.println(Arrays.toString(arr));				
-//			}
-//			System.out.println();
-		}
+		visited = new int[N][M];
+		bfs();
 		max_result = Integer.MIN_VALUE;
 		for(int r = 0; r < N; r++) {
 			for(int c = 0; c < M; c++) {
-				max_result = Math.max(step[r][c],max_result);
+				max_result = Math.max(max_result, step[r][c]);
 			}			
 		}
 		
@@ -82,15 +76,20 @@ public class Main {
 	static int[] dr = {-1,-1,0,1,1,1,0,-1};
 	static int[] dc = {0,1,1,1,0,-1,-1,-1};
 	
-	public static void bfs(Shark start) {
-		visited = new int[N][M];
+	public static void bfs() {
 		// 1부터 시작해서 visited까지 한꺼번에 처리하고 마지막에 -1빼고 계산
-		step[start.r][start.c] = 0;
-		visited[start.r][start.c] = 1;
-		Queue<Shark> q = new LinkedList<>();
-		q.add(start);
+		Queue<RC> q = new LinkedList<>();
+		for(int i = 0, size = slist.size(); i < size; i++) {
+			RC cur = slist.get(i);
+			int curr = cur.r;
+			int curc = cur.c;
+			step[curr][curc] = 0;
+			visited[curr][curc] = 1;			
+			q.add(cur);
+		}
+		
 		while(q.size()>0) {
-			Shark cur = q.poll();
+			RC cur = q.poll();
 			int curr = cur.r;
 			int curc = cur.c;
 			
@@ -98,9 +97,9 @@ public class Main {
 				int nr = curr+dr[d];
 				int nc = curc+dc[d];
 				if(inRange(nr,nc) && visited[nr][nc]==0) {	
-					step[nr][nc] = Math.min(step[nr][nc], step[curr][curc]+1);
+					step[nr][nc] = step[curr][curc]+1;
 					visited[nr][nc]=1;
-					q.add(new Shark(nr,nc));
+					q.add(new RC(nr,nc));
 				}
 			}
 		}
