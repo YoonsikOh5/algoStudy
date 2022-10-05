e2164f350deb71f417f1f8695dc7fb4c1d4edfc9
YoonsikOh5 <109320448+YoonsikOh5@users.noreply.github.com>
2022-10-05 13:21:47 +0900

    [Bronze V] Title: A/B, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/1008. A／B/A／B.cc
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+    int a;
+    int b;
+    
+    cin >> a;
+    cin >> b;
+    
+    double c;
+    
+    c = (double) a/b;
+    
+    cout.precision(10);
+    cout << c;
+
+    return 0;
+}
