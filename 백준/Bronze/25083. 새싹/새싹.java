0d32eb4734862c195dfc6fa7578a80eaf28fa48d
YoonsikOh5 <109320448+YoonsikOh5@users.noreply.github.com>
2022-11-11 16:17:27 +0900

    [Bronze V] Title: 새싹, Time: 72 ms, Memory: 11320 KB -BaekjoonHub

---
+++ 백준/Bronze/25083. 새싹/새싹.java
@@ -0,0 +1,29 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+public class Main {
+
+
+
+	public static void main(String[] args) throws IOException {
+
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+		
+		bw.write("         ,r'\"7\n" + 
+				"r`-_   ,'  ,/\n" + 
+				" \\. \". L_r'\n" + 
+				"   `~\\/\n" + 
+				"      |\n" + 
+				"      |");
+		bw.flush();
+		bw.close();
+
+	}
+
+	
+
+}
