e5b37fa76da4fda6ccf9840c913c4b5db34e8d85
YoonsikOh5 <109320448+YoonsikOh5@users.noreply.github.com>
2022-11-28 17:12:03 +0900

    [Bronze V] Title: 사파리월드, Time: 80 ms, Memory: 11632 KB -BaekjoonHub

---
+++ 백준/Bronze/2420. 사파리월드/사파리월드.java
@@ -0,0 +1,28 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+
+public class Main {
+
+	public static void main(String[] args) throws NumberFormatException, IOException {
+
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		
+		long N = Long.parseLong(st.nextToken());
+		long M = Long.parseLong(st.nextToken());
+		long result = Math.abs(N-M);
+		
+		bw.write(result+"");
+		bw.flush();
+		bw.close();
+		br.close();
+		
+	}
+
+}
