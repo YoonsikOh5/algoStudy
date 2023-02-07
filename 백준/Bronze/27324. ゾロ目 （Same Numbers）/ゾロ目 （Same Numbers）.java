3bb9942b889867f14fc1e87f760cd977c986a599
YoonsikOh5 <109320448+YoonsikOh5@users.noreply.github.com>
2023-02-08 08:53:37 +0900

    [Bronze V] Title: ゾロ目 (Same Numbers), Time: 128 ms, Memory: 14272 KB -BaekjoonHub

---
+++ 백준/Bronze/27324. ゾロ目 （Same Numbers）/ゾロ目 （Same Numbers）.java
@@ -0,0 +1,27 @@
+import java.io.*;
+import java.util.*;
+
+
+public class Main {
+
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        String str = br.readLine();
+
+        if(str.charAt(0)==str.charAt(1)){
+            bw.write(1+"");
+        } else {
+            bw.write(0+"");
+        }
+        bw.flush();
+        bw.close();
+        br.close();
+
+    }
+
+
+}
