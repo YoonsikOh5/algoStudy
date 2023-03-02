5b767cc2c5f3f1c2c7587ef665ef88881d67529c
YoonsikOh5 <109320448+YoonsikOh5@users.noreply.github.com>
2023-03-02 13:06:51 +0900

    [Bronze V] Title: 코딩은 체육과목 입니다, Time: 96 ms, Memory: 12952 KB -BaekjoonHub

---
+++ 백준/Bronze/25314. 코딩은 체육과목 입니다/코딩은 체육과목 입니다.kt
@@ -0,0 +1,10 @@
+
+fun main(){
+    val readLine: String? = readLine()
+    val a = readLine!!.toInt()
+    val num = a / 4
+    for(i in 1..num){
+        print("long ")
+    }
+    println("int")
+}
