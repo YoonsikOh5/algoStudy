e28121c05c512ec657dd6d97c40518100bf96dd8
YoonsikOh5 <109320448+YoonsikOh5@users.noreply.github.com>
2023-03-19 10:38:23 +0900

    [Bronze V] Title: 홍익대학교, Time: 204 ms, Memory: 17684 KB -BaekjoonHub

---
+++ 백준/Bronze/16394. 홍익대학교/홍익대학교.java
@@ -0,0 +1,28 @@
+import java.awt.desktop.SystemEventListener;
+import java.io.*;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Scanner;
+import java.util.StringTokenizer;
+
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+
+//        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+//        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        Scanner sc = new Scanner(System.in);
+
+        int N = sc.nextInt();
+
+        System.out.println((N-1946));
+
+//        bw.flush();
+//        bw.close();
+//        br.close();
+
+    }
+
+}
