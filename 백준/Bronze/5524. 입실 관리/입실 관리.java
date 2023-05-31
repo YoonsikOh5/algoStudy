27b32bc552f1b1028b1877a2a5462abc985e7fa3
YoonsikOh5 <109320448+YoonsikOh5@users.noreply.github.com>
2023-05-31 13:15:09 +0900

    [Bronze IV] Title: 입실 관리, Time: 128 ms, Memory: 14260 KB -BaekjoonHub

---
+++ 백준/Bronze/5524. 입실 관리/입실 관리.java
@@ -0,0 +1,23 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int N = Integer.parseInt(br.readLine());
+
+        for (int i = 1; i <= N; i++) {
+            String str = br.readLine();
+            String s = str.toLowerCase(Locale.ROOT);
+            bw.write(s + "\n");
+        }
+
+
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+}
