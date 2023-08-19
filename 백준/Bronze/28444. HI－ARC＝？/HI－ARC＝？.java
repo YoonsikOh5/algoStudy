c2400bfd65d93a66170b86e3a0b14c2bc23156d5
YoonsikOh5 <109320448+YoonsikOh5@users.noreply.github.com>
2023-08-19 18:06:23 +0900

    [Bronze V] Title: HI-ARC=?, Time: 140 ms, Memory: 14256 KB -BaekjoonHub

---
+++ 백준/Bronze/28444. HI－ARC＝？/HI－ARC＝？.java
@@ -0,0 +1,28 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) throws NumberFormatException, IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int H = Integer.parseInt(st.nextToken());
+        int I = Integer.parseInt(st.nextToken());
+        int A = Integer.parseInt(st.nextToken());
+        int R = Integer.parseInt(st.nextToken());
+        int C = Integer.parseInt(st.nextToken());
+
+        int a = H * I;
+        int b = A * R * C;
+        int result = a - b;
+
+        bw.write(result+"");
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+
+}
