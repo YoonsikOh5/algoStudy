31cb83cca081f3264616d81c2d02929c2da47317
YoonsikOh5 <109320448+YoonsikOh5@users.noreply.github.com>
2023-10-29 22:00:00 +0900

    [Platinum V] Title: 히스토그램에서 가장 큰 직사각형, Time: 632 ms, Memory: 179816 KB -BaekjoonHub

---
+++ 백준/Platinum/6549. 히스토그램에서 가장 큰 직사각형/히스토그램에서 가장 큰 직사각형.java
@@ -0,0 +1,124 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    static long[] arr;
+
+    static int N;
+
+    static Node[] minTree;
+
+    static long result;
+
+    static class Node {
+        long minHeight;
+        int minIndex;
+
+        public Node(long minHeight, int minIndex) {
+            this.minHeight = minHeight;
+            this.minIndex = minIndex;
+        }
+
+        @Override
+        public String toString() {
+            return "Node{" +
+                    "minHeight=" + minHeight +
+                    ", minIndex=" + minIndex +
+                    '}';
+        }
+    }
+
+    public static void main(String args[]) throws Exception {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        N = Integer.parseInt(st.nextToken());
+
+        while (N != 0) {
+            arr = new long[N + 1];
+            minTree = new Node[N * 4 + 4];
+            result = 0;
+
+            for (int i = 1; i <= N; i++) {
+                arr[i] = Long.parseLong(st.nextToken());
+            }
+
+            init(1, 1, N);
+            findAnswer(1, N);
+
+            bw.write(result + "\n");
+
+            st = new StringTokenizer(br.readLine());
+            N = Integer.parseInt(st.nextToken());
+        }
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+
+    public static void init(int node, int nodeLeft, int nodeRight) {
+        if (nodeLeft == nodeRight) {
+            minTree[node] = new Node(arr[nodeLeft], nodeLeft);
+            return;
+        }
+
+        int mid = (nodeLeft + nodeRight) / 2;
+
+        init(node * 2, nodeLeft, mid);
+        init(node * 2 + 1, mid + 1, nodeRight);
+
+        Node leftChild = minTree[node * 2];
+        Node rightChild = minTree[node * 2 + 1];
+
+        if (leftChild.minHeight <= rightChild.minHeight) {
+            minTree[node] = leftChild;
+        } else {
+            minTree[node] = rightChild;
+        }
+    }
+
+    public static Node queryMin(int node, int nodeLeft, int nodeRight, int queryLeft, int queryRight) {
+        if (queryRight < nodeLeft || nodeRight < queryLeft) {
+            return new Node(1000000001, 0);
+        }
+
+        if (queryLeft <= nodeLeft && nodeRight <= queryRight) {
+            return minTree[node];
+        }
+
+        int mid = (nodeLeft + nodeRight) / 2;
+        Node leftQuery = queryMin(node * 2, nodeLeft, mid, queryLeft, queryRight);
+        Node rightQuery = queryMin(node * 2 + 1, mid + 1, nodeRight, queryLeft, queryRight);
+
+        if (leftQuery.minHeight <= rightQuery.minHeight) {
+            return leftQuery;
+        } else {
+            return rightQuery;
+        }
+    }
+
+    public static void findAnswer(int queryLeft, int queryRight) {
+        if (queryLeft > queryRight) {
+            return;
+        }
+
+        Node minNode = queryMin(1, 1, N, queryLeft, queryRight);
+        result = Math.max(result, minNode.minHeight * (queryRight - queryLeft + 1));
+
+        if (queryLeft == queryRight) {
+            return;
+        }
+
+        if (queryLeft < minNode.minIndex) {
+            findAnswer(queryLeft, minNode.minIndex - 1);
+        }
+        if (minNode.minIndex < queryRight) {
+            findAnswer(minNode.minIndex + 1, queryRight);
+        }
+    }
+
+}
