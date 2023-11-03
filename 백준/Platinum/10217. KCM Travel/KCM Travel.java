4eca4b74daaf78aafb0f8bfb00147000f5967c39
YoonsikOh5 <109320448+YoonsikOh5@users.noreply.github.com>
2023-11-03 13:11:38 +0900

    [Platinum V] Title: KCM Travel, Time: 1536 ms, Memory: 122060 KB -BaekjoonHub

--- 백준/Platinum/10217. KCM Travel/KCM Travel.java
+++ 백준/Platinum/10217. KCM Travel/KCM Travel.java
@@ -4,125 +4,139 @@ import java.util.*;
 public class Main {
 
     static int N, M, K;
-
-    // dp
+    // dp[i][j] i원을 들고 j를 갈때의 최소거리
     static int[][] dp;
-    static boolean[][] visited;
-    static List<Edge>[] els;
+    static List<Edge>[] edgeLs;
 
-    static class Edge implements Comparable<Edge> {
-        int curCost;
-        int left;
-        int right;
-        int price;
-        int len;
+    static class Edge {
+        int start;
+        int end;
+        int cost;
+        int dist;
 
-        public Edge(int left, int right, int price, int len) {
-            this.left = left;
-            this.right = right;
-            this.price = price;
-            this.len = len;
+        public Edge(int start, int end, int cost, int dist) {
+            this.start = start;
+            this.end = end;
+            this.cost = cost;
+            this.dist = dist;
         }
 
         @Override
-        public int compareTo(Edge o) {
-            if (this.len == o.len) {
-                return this.curCost - o.curCost;
-            }
-            return this.len - o.len;
+        public String toString() {
+            return "Edge{" +
+                    "start=" + start +
+                    ", end=" + end +
+                    ", cost=" + cost +
+                    ", dist=" + dist +
+                    '}';
         }
     }
 
-    public static void main(String[] args) throws IOException {
+    public static void main(String args[]) throws Exception {
+
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
+
         int T = Integer.parseInt(br.readLine());
 
-        for (int tc = 1; tc <= T; tc++) {
-            StringTokenizer st = new StringTokenizer(br.readLine());
 
+        for (int tc = 1; tc <= T; tc++) {
+
+            StringTokenizer st = new StringTokenizer(br.readLine());
             N = Integer.parseInt(st.nextToken());
             M = Integer.parseInt(st.nextToken());
             K = Integer.parseInt(st.nextToken());
-
-            els = new List[N + 1];
-
-            dp = new int[N + 1][M + 1];
-            visited = new boolean[N + 1][M + 1];
-
-            for (int i = 0; i <= N; i++) {
-                for (int j = 0; j <= M; j++) {
-                    dp[i][j] = Integer.MAX_VALUE;
-                }
+            edgeLs = new List[N + 1];
+            for (int n = 1; n <= N; n++) {
+                edgeLs[n] = new LinkedList<>();
             }
 
-            for (int i = 0; i <= N; i++) {
-                els[i] = new LinkedList<Edge>();
-            }
-
-            for (int i = 1; i <= K; i++) {
+            for (int k = 1; k <= K; k++) {
                 st = new StringTokenizer(br.readLine());
-                int left = Integer.parseInt(st.nextToken());
-                int right = Integer.parseInt(st.nextToken());
-                int price = Integer.parseInt(st.nextToken());
-                int len = Integer.parseInt(st.nextToken());
-                els[left].add(new Edge(left, right, price, len));
+                int u = Integer.parseInt(st.nextToken());
+                int v = Integer.parseInt(st.nextToken());
+                int c = Integer.parseInt(st.nextToken());
+                int d = Integer.parseInt(st.nextToken());
+                edgeLs[u].add(new Edge(u, v, c, d));
             }
-            int result = dijk();
-            if (result != -1) {
-                bw.write(result + "\n");
-            } else if (result == -1) {
+
+            for(int n = 1; n <= N; n++){
+                edgeLs[n].sort(new Comparator<Edge>() {
+                    @Override
+                    public int compare(Edge o1, Edge o2) {
+                        return o1.dist - o2.dist;
+                    }
+                });
+            }
+
+
+
+            dijk();
+            int minResult = Integer.MAX_VALUE;
+            for (int m = 0; m <= M; m++) {
+                minResult = Math.min(minResult, dp[m][N]);
+            }
+            if (minResult == Integer.MAX_VALUE) {
                 bw.write("Poor KCM\n");
+            } else {
+                bw.write(minResult + "\n");
             }
         }
-
-
         bw.flush();
         bw.close();
         br.close();
     }
 
-    private static int dijk() {
+    private static void dijk() {
+        dp = new int[M + 1][N + 1];
 
-        PriorityQueue<Edge> pq = new PriorityQueue<>();
-
-        pq.offer(new Edge(1, 1, 0, 0));
-
-        for (int i = 0; i <= M; i++) {
-            dp[1][i] = 0;
+        for (int m = 0; m <= M; m++) {
+            for (int n = 2; n <= N; n++) {
+                dp[m][n] = Integer.MAX_VALUE;
+            }
         }
 
-        for (int i = 1; i <= M; i++) {
-            visited[1][i] = true;
-        }
+        Queue<Edge> q = new LinkedList<>();
 
-        while (pq.size() > 0) {
-            Edge poll = pq.poll();
+        q.offer(new Edge(1, 1, 0, 0));
 
-            int cost = poll.curCost;
-            int left = poll.left;
-            int right = poll.right;
-            int len = poll.len;
-
-            if (visited[right][cost] == true) {
+        while (q.size() > 0) {
+            Edge poll = q.poll();
+            int pe = poll.end;
+            int pd = poll.dist;
+            int pc = poll.cost;
+            if (pe == N) continue;
+            if (pd > dp[pc][pe]) {
                 continue;
             }
-            visited[right][cost] = true;
-            if (right == N) {
-                return len;
-            }
-
-            List<Edge> el = els[right];
-            for (Edge edge : el) {
-                if (cost + edge.price <= M && dp[edge.right][cost + edge.price] > len + edge.len) {
-                    dp[edge.right][cost + edge.price] = len + edge.len;
-                    Edge wEdge = new Edge(edge.left, edge.right, 0, len + edge.len);
-                    wEdge.curCost = cost + edge.price;
-                    pq.offer(wEdge);
+            List<Edge> edgeL = edgeLs[pe];
+            outLoop:
+            for (Edge edge : edgeL) {
+                int ee = edge.end;
+                int ed = edge.dist;
+                int ec = edge.cost;
+                int cc = pc + ec;
+                if (cc > M) {
+                    continue;
                 }
+                int cd = pd + ed;
+
+                if (cd >= dp[cc][ee]) {
+                    continue;
+                }
+
+                for (int m = cc+1; m <= M; m++) {
+                    if (dp[m][ee] > cd) {
+                        dp[m][ee] = cd;
+                    } else if (dp[m][ee] <= cd) {
+                        break;
+                    }
+                }
+                dp[cc][ee] = cd;
+                q.offer(new Edge(pe, ee, cc, cd));
+
             }
         }
-        return -1;
     }
 }
