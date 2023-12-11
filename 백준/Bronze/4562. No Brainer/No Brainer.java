4eb2a7f7740dc05fa3cd6dc99ef2224e8cfdefa2
YoonsikOh5 <109320448+YoonsikOh5@users.noreply.github.com>
2023-12-11 11:31:35 +0900

    [Bronze IV] Title: No Brainer, Time: 116 ms, Memory: 14136 KB -BaekjoonHub

---
+++ 백준/Bronze/4562. No Brainer/No Brainer.java
@@ -0,0 +1,30 @@
+import java.io.*;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    public static void main(String[] args) throws NumberFormatException, IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int N = Integer.parseInt(br.readLine());
+
+        for (int i = 0; i < N; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+            if (a >= b) {
+                bw.write("MMM BRAINS\n");
+            } else {
+                bw.write("NO BRAINS\n");
+            }
+        }
+
+
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+
+}
