c87d3fed407098385fb7029b1e4a63093cefb161
YoonsikOh5 <109320448+YoonsikOh5@users.noreply.github.com>
2023-12-23 22:02:34 +0900

    [Bronze IV] Title: 帰省 (Homecoming), Time: 124 ms, Memory: 14148 KB -BaekjoonHub

---
+++ 백준/Bronze/24072. 帰省 （Homecoming）/帰省 （Homecoming）.java
@@ -0,0 +1,28 @@
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    public static void main(String[] args) throws NumberFormatException, IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int a = Integer.parseInt(st.nextToken());
+        int b = Integer.parseInt(st.nextToken());
+        int c = Integer.parseInt(st.nextToken());
+
+        if (a <= c && c < b) {
+            bw.write("1");
+        } else {
+            bw.write("0");
+        }
+
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+
+}
