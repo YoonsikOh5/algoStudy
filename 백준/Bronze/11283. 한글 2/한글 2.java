8ee1c15070efcf9b4af383db325e357c9aed0f76
YoonsikOh5 <109320448+YoonsikOh5@users.noreply.github.com>
2024-01-02 07:35:51 +0900

    [Bronze IV] Title: 한글 2, Time: 136 ms, Memory: 14332 KB -BaekjoonHub

---
+++ 백준/Bronze/11283. 한글 2/한글 2.java
@@ -0,0 +1,24 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) throws NumberFormatException, IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        char c = br.readLine().charAt(0);
+
+        bw.write((c-'가'+1)+"");
+
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+
+}
