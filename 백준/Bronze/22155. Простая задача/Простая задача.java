80a466b3a0893216ce49ca6b1e9700408d8db02d
YoonsikOh5 <109320448+YoonsikOh5@users.noreply.github.com>
2024-01-27 21:50:35 +0900

    [Bronze IV] Title: Простая задача, Time: 120 ms, Memory: 14288 KB -BaekjoonHub

---
+++ 백준/Bronze/22155. Простая задача/Простая задача.java
@@ -0,0 +1,33 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) throws NumberFormatException, IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int T = Integer.parseInt(br.readLine());
+
+        for (int t = 1; t <= T; t++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int n = Integer.parseInt(st.nextToken());
+            int m = Integer.parseInt(st.nextToken());
+            if (!(n <= 2 && m <= 1) && !(n <= 1 && m <= 2)) {
+                bw.write("No\n");
+            } else {
+                bw.write("Yes\n");
+            }
+        }
+
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+
+}
