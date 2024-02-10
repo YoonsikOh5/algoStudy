3f773260c7a8abaf025e080e8635c800e23ae9fb
YoonsikOh5 <109320448+YoonsikOh5@users.noreply.github.com>
2024-02-11 08:10:24 +0900

    [Bronze III] Title: 별 찍기 - 13, Time: 128 ms, Memory: 14208 KB -BaekjoonHub

---
+++ 백준/Bronze/2523. 별 찍기 － 13/별 찍기 － 13.java
@@ -0,0 +1,38 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) throws NumberFormatException, IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int n = Integer.parseInt(br.readLine());
+
+        for (int i = 1; i <= n; i++) {
+            int star = i;
+            for (int s = 1; s <= star; s++) {
+                bw.write("*");
+            }
+            bw.write("\n");
+        }
+
+        for (int i = n - 1; i >= 1; i--) {
+            int star = i;
+            for (int s = 1; s <= star; s++) {
+                bw.write("*");
+            }
+            bw.write("\n");
+        }
+
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+
+}
