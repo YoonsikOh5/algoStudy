34b77f66a8de03a18926c084e38c9d3daf167321
YoonsikOh5 <109320448+YoonsikOh5@users.noreply.github.com>
2024-02-20 20:40:36 +0900

    [Bronze IV] Title: ЗЛАТНАТА СРЕДА, Time: 124 ms, Memory: 14248 KB -BaekjoonHub

---
+++ 백준/Bronze/31281. ЗЛАТНАТА СРЕДА/ЗЛАТНАТА СРЕДА.java
@@ -0,0 +1,32 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) throws NumberFormatException, IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        long a = Long.parseLong(st.nextToken());
+        long b = Long.parseLong(st.nextToken());
+        long c = Long.parseLong(st.nextToken());
+
+        PriorityQueue<Long> pq = new PriorityQueue<>();
+        pq.offer(a);
+        pq.offer(b);
+        pq.offer(c);
+        pq.poll();
+        bw.write(pq.poll()+"");
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+
+}
