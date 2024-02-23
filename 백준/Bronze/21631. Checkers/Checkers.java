ae5ac432b6dea9b61222fff77b06a51d80060514
YoonsikOh5 <109320448+YoonsikOh5@users.noreply.github.com>
2024-02-23 19:31:25 +0900

    [Bronze IV] Title: Checkers, Time: 124 ms, Memory: 14284 KB -BaekjoonHub

---
+++ 백준/Bronze/21631. Checkers/Checkers.java
@@ -0,0 +1,32 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) throws NumberFormatException, IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        long a = Long.parseLong(st.nextToken());
+        long b = Long.parseLong(st.nextToken());
+
+        if(a >= b){
+            bw.write(b+"");
+        } else if(a < b){
+            bw.write((a+1)+"");
+        }
+
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+
+}
