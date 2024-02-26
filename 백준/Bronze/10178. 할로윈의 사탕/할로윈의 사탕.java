5d1acec8a685f3eb71e2bf5e9a73aff94ba58867
YoonsikOh5 <109320448+YoonsikOh5@users.noreply.github.com>
2024-02-26 22:09:35 +0900

    [Bronze III] Title: 할로윈의 사탕, Time: 140 ms, Memory: 15900 KB -BaekjoonHub

---
+++ 백준/Bronze/10178. 할로윈의 사탕/할로윈의 사탕.java
@@ -0,0 +1,31 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) throws NumberFormatException, IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int N = Integer.parseInt(br.readLine());
+
+        for (int i = 1; i <= N; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+            int div = a / b;
+            int rem = a % b;
+            bw.write("You get "+div+" piece(s) and your dad gets "+rem+" piece(s).\n");        }
+
+
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+
+}
