5bfe28ada6f76062311c1620c6132386363b98af
YoonsikOh5 <109320448+YoonsikOh5@users.noreply.github.com>
2025-07-14 22:27:19 +0900

    [Bronze IV] Title: 다면체, Time: 136 ms, Memory: 14504 KB -BaekjoonHub

---
+++ 백준/Bronze/10569. 다면체/다면체.java
@@ -0,0 +1,31 @@
+import java.io.*;
+import java.text.DecimalFormat;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int T = Integer.parseInt(br.readLine());
+
+        for(int i = 0; i < T; i++){
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+            int res = 2 + b - a;
+            bw.write(res+"\n");
+        }
+
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+
+}
