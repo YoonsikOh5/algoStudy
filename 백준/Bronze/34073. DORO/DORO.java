751841f5a559e06cbae98db5ea5827f3216592b6
YoonsikOh5 <109320448+YoonsikOh5@users.noreply.github.com>
2025-07-28 22:00:11 +0900

    [Bronze IV] Title: DORO, Time: 104 ms, Memory: 14276 KB -BaekjoonHub

---
+++ 백준/Bronze/34073. DORO/DORO.java
@@ -0,0 +1,25 @@
+import java.io.*;
+import java.math.BigInteger;
+import java.text.DecimalFormat;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int N = Integer.parseInt(br.readLine());
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for(int i = 1; i <= N; i++){
+            bw.write(st.nextToken()+"DORO ");
+        }
+
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+
+}
