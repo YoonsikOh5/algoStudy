f9ebccde2bd6c590ddde4b2f253f437f00a97f54
YoonsikOh5 <109320448+YoonsikOh5@users.noreply.github.com>
2025-09-08 23:03:51 +0900

    [Bronze V] Title: Class Photo, Time: 100 ms, Memory: 14120 KB -BaekjoonHub

---
+++ 백준/Bronze/34281. Class Photo/Class Photo.java
@@ -0,0 +1,22 @@
+import java.io.*;
+import java.math.BigInteger;
+import java.text.DecimalFormat;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int a = Integer.parseInt(br.readLine());
+        int b = Integer.parseInt(br.readLine());
+        int res = a*b;
+        bw.write(res + "");
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+
+}
