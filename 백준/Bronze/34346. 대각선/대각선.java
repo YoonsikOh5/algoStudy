3692d56c4efb4b620f7cd34342b78e69cf5b4e1d
YoonsikOh5 <109320448+YoonsikOh5@users.noreply.github.com>
2025-09-15 06:13:52 +0900

    [Bronze IV] Title: 대각선, Time: 100 ms, Memory: 14300 KB -BaekjoonHub

---
+++ 백준/Bronze/34346. 대각선/대각선.java
@@ -0,0 +1,25 @@
+import java.io.*;
+import java.math.BigInteger;
+import java.text.DecimalFormat;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int N = Integer.parseInt(br.readLine());
+        if(N % 2 == 0){
+            bw.write("2");
+        } else {
+            bw.write("1");
+        }
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+
+}
