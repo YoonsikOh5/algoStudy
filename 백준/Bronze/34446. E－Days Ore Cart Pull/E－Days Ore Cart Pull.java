0337715da3b5ed8468e4b58a7372fa328b6b8ff0
YoonsikOh5 <109320448+YoonsikOh5@users.noreply.github.com>
2025-09-23 22:37:46 +0900

    [Bronze V] Title: E-Days Ore Cart Pull, Time: 96 ms, Memory: 14228 KB -BaekjoonHub

---
+++ 백준/Bronze/34446. E－Days Ore Cart Pull/E－Days Ore Cart Pull.java
@@ -0,0 +1,24 @@
+import java.io.*;
+import java.math.BigInteger;
+import java.text.DecimalFormat;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int a = Integer.parseInt(br.readLine());
+        int b = Integer.parseInt(br.readLine());
+        int c = Integer.parseInt(br.readLine());
+        int res = a*2;
+        bw.write(res+"");
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+
+}
