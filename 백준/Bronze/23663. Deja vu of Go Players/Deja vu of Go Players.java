07d6b6b5e074e7ddeeb144e52f1de7d3d917b013
YoonsikOh5 <109320448+YoonsikOh5@users.noreply.github.com>
2025-10-21 23:43:18 +0900

    [Bronze III] Title: Deja vu of Go Players, Time: 108 ms, Memory: 14852 KB -BaekjoonHub

---
+++ 백준/Bronze/23663. Deja vu of Go Players/Deja vu of Go Players.java
@@ -0,0 +1,34 @@
+import java.io.*;
+import java.math.BigInteger;
+import java.text.DecimalFormat;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int T = Integer.parseInt(br.readLine());
+
+        for(int i = 1; i <= T; i++){
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int n = Integer.parseInt(st.nextToken());
+            int m = Integer.parseInt(st.nextToken());
+
+            br.readLine();
+            br.readLine();
+            if(n <= m){
+                bw.write("Yes\n");
+            } else {
+                bw.write("No\n");
+            }
+        }
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+
+}
