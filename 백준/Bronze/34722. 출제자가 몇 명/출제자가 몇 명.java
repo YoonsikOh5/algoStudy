b5ba37fef8788fef163fb264d4b63870bf3034b2
YoonsikOh5 <109320448+YoonsikOh5@users.noreply.github.com>
2025-11-10 23:11:50 +0900

    [Bronze IV] Title: 출제자가 몇 명, Time: 112 ms, Memory: 14324 KB -BaekjoonHub

---
+++ 백준/Bronze/34722. 출제자가 몇 명/출제자가 몇 명.java
@@ -0,0 +1,32 @@
+import java.io.*;
+import java.math.BigInteger;
+import java.text.DecimalFormat;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int N = Integer.parseInt(br.readLine());
+        int cnt = 0;
+        for (int i = 0; i < N; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+            int c = Integer.parseInt(st.nextToken());
+            int d = Integer.parseInt(st.nextToken());
+            if(a >= 1000 || b >= 1600 || c >= 1500 || (d >= 1 && d <= 30)){
+                cnt++;
+            }
+        }
+        bw.write(cnt+"");
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+
+}
