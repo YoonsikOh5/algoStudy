61beaa2693afc23e8723a63fb76a7771cf46283e
YoonsikOh5 <109320448+YoonsikOh5@users.noreply.github.com>
2025-11-15 09:27:57 +0900

    [Bronze III] Title: 사과, Time: 104 ms, Memory: 14480 KB, Score: 100 point -BaekjoonHub

---
+++ 백준/Bronze/10833. 사과/사과.java
@@ -0,0 +1,32 @@
+import java.io.*;
+import java.math.BigInteger;
+import java.text.DecimalFormat;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int N = Integer.parseInt(br.readLine());
+
+        int res = 0;
+        for (int i = 0; i < N; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+            int cur = b % a;
+            res += cur;
+        }
+
+
+        bw.write(res + "");
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+
+}
