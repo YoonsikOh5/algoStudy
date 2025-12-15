916c6bb67393db3fbc322b1a67ec55fa05f17f06
YoonsikOh5 <109320448+YoonsikOh5@users.noreply.github.com>
2025-12-16 08:01:23 +0900

    [Bronze III] Title: Abdelrahman, Time: 120 ms, Memory: 16156 KB -BaekjoonHub

---
+++ 백준/Bronze/10693. Abdelrahman/Abdelrahman.java
@@ -0,0 +1,26 @@
+import java.io.*;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int N = Integer.parseInt(br.readLine());
+
+        for(int i = 1; i <= N; i++){
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int A = Integer.parseInt(st.nextToken());
+            int B = Integer.parseInt(st.nextToken());
+            int res = B - (A - 1);
+            bw.write("Case "+i+": "+res+"\n");
+        }
+
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+
+}
