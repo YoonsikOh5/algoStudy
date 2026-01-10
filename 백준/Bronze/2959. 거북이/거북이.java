7b42056c394d9f29c53bd798fe5909397fb0d8e6
YoonsikOh5 <109320448+YoonsikOh5@users.noreply.github.com>
2026-01-10 22:07:38 +0900

    [Bronze III] Title: 거북이, Time: 92 ms, Memory: 14224 KB -BaekjoonHub

---
+++ 백준/Bronze/2959. 거북이/거북이.java
@@ -0,0 +1,33 @@
+import java.io.*;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int a = Integer.parseInt(st.nextToken());
+        int b = Integer.parseInt(st.nextToken());
+        int c = Integer.parseInt(st.nextToken());
+        int d = Integer.parseInt(st.nextToken());
+
+        int[] arr = new int[4];
+        arr[0] = a;
+        arr[1] = b;
+        arr[2] = c;
+        arr[3] = d;
+
+        Arrays.sort(arr);
+
+        int res = arr[0] * arr[2];
+        bw.write(res+"");
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+
+}
