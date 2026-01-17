a2992a185708639786c9e2784ffa2963421037ce
YoonsikOh5 <109320448+YoonsikOh5@users.noreply.github.com>
2026-01-18 00:56:18 +0900

    [Bronze III] Title: Internetas, Time: 116 ms, Memory: 14512 KB -BaekjoonHub

---
+++ 백준/Bronze/7281. Internetas/Internetas.java
@@ -0,0 +1,34 @@
+import java.io.*;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int N = Integer.parseInt(br.readLine());
+
+        int bef = 0;
+        int res = 0;
+        for(int i = 1; i <= N; i++){
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+
+            if(b==1){
+                res = Math.max(res, (a-bef));
+                bef = a;
+            }
+
+        }
+
+        bw.write(res+"");
+
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+
+}
