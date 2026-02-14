702160f225bd14ec9c3272098a14f31d57404a51
YoonsikOh5 <109320448+YoonsikOh5@users.noreply.github.com>
2026-02-14 10:47:15 +0900

    [Bronze III] Title: Изгороди, Time: 100 ms, Memory: 14240 KB -BaekjoonHub

---
+++ 백준/Bronze/24623. Изгороди/Изгороди.java
@@ -0,0 +1,18 @@
+import java.io.*;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        bw.write("90");
+
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+
+}
