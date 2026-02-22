f3cf6b16bef5b2b2e3d943e71059f042c1adf1de
YoonsikOh5 <109320448+YoonsikOh5@users.noreply.github.com>
2026-02-22 22:44:48 +0900

    [Bronze IV] Title: 유림이와 하람이의 두쫀쿠 대작전, Time: 112 ms, Memory: 14248 KB -BaekjoonHub

---
+++ 백준/Bronze/35277. 유림이와 하람이의 두쫀쿠 대작전/유림이와 하람이의 두쫀쿠 대작전.java
@@ -0,0 +1,20 @@
+import java.io.*;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int N = Integer.parseInt(br.readLine());
+        int res = N / 2000;
+
+        bw.write(res+"");
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+
+}
